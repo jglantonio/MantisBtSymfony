368c6d7d658fb016ff3fabcb451ddab8c074565e
jglantonio <joseantoniogonzalezlopez@gmail.com>
2026-09-19 14:07:14 +0200

    Test ok Bug Controller

--- tests/Controller/BugControllerTest.php
+++ tests/Controller/BugControllerTest.php
@@ -6,11 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class BugControllerTest extends WebTestCase
 {
-    public function testIndex(): void
+    public static function dataProviderUrls(){
+        yield "Bug" => ['/bug','/'];
+        yield "Bug > Create" => ['/bug/create','/'];
+        yield "Bug > Edit" => ['/bug/edit/1','/'];
+        yield "Bug > Show > id" => ['/bug/show/1','/'];
+    }
+
+    /**
+     * @dataProvider dataProviderUrls
+     * @return void
+     */
+    public function testAccessWithOutLogin($from,$to): void
     {
         $client = static::createClient();
-        $client->request('GET', '/bug');
-
-        self::assertResponseIsSuccessful();
+        $client->request('GET', $from);
+        self::assertResponseRedirects($to);
     }
 }
